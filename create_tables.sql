9b5f9201b4ea3e53e58b480f40fd04c31a4d6275
Stéphanie MOALLIC <stephanie.moallic@gmail.com>
2016-11-27 23:44:51 +0100

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -62,7 +62,7 @@ RESPONSABLE  varchar2(30) invisible
 create table DEPOTS(CODE char(2) primary key, ADRESSE varchar2(60));
 
 create table EMPLOYES(
-  NUMERO number(3) generated as identity,
+  NUMERO number(3),
   NOM    varchar2(30),
   AGE      number(3),
   SALAIRE  number(10)
