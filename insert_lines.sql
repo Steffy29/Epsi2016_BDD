9442e907180d0f1fa24587b4d7400ee7bdd32eca
Stéphanie MOALLIC <stephanie.moallic@gmail.com>
2016-11-27 21:07:32 +0100

    Update insert_lines.sql

--- insert_lines.sql
+++ insert_lines.sql
@@ -2,7 +2,21 @@ insert into CLIENTS(NOCLI, NOMCLI)
 values (37, ‘Ets LAROCHE’);
 
 insert into ARTICLES
-values ('AB03', 'Carpettes', 150, 2, 'IMPORT', 80);
+values ('AB22', 'Tapis Persan', 1250.1, 2, 'IMPORT', 5);
+insert into ARTICLES
+values ('CD50', 'Chaine HiFi', 735.4, 2, 'IMPORT', 7);
+insert into ARTICLES
+values ('ZZZZ', 'Article bidon', null, null, 'DIVERS', 25);
+insert into ARTICLES
+values ('AA00', 'Cadeau', 0, null, 'DIVERS', 8);
+insert into ARTICLES
+values ('AB03', 'Carpettes', 150, 2, 'SOLDES', 116);
+insert into ARTICLES
+values ('AB', 'Tapis', null, 2, 'DIVERS', 2);
+insert into ARTICLES
+values ('ZZ01', 'Lot de tapis', 500, 2, 'DIVERS', 0);
+insert into ARTICLES
+values ('AB10', 'Tapis de Chine', 1500, 2, 'IMPORT', 10);
 
 insert into LIGCDES
 values (&NumCde, 1, 'AB10', 2+6);
